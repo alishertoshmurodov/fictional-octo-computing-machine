b104b2d01b7db63391ec75381e928232b6d4bbf1
Alisher Toshmurodov <toshmurodov2001@mail.ru>
2022-08-04 11:25:23 +0500

    beta

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿string[] arr = { "hello", "world", "!", "My", "name", "is", "Alisher!" };
+
+List<string> list = new List<string>();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        list.Add(arr[i]);
+    }
+}
+
+string[] result = list.ToArray();
+
+Console.WriteLine("["  + string.Join(", ", result) + "]");
