8fcf13d8ac6cbc525147471e4caaf99d2c8ae289
Alisher Toshmurodov <toshmurodov2001@mail.ru>
2022-08-04 11:40:20 +0500

    Final

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿string[] arr = { "hello", "world", "!", "My", "name", "is", "Alisher!" };
+﻿string[] InputByUser(){
+    Console.WriteLine("Number of elements in your array:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[n];
+
+    int c = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Element number {0}", c);
+        array[i] = Console.ReadLine();
+        c++;
+    }
+    return array;
+}
+
+
+string[] byuser = InputByUser();
 
 List<string> list = new List<string>();
 
-for (int i = 0; i < arr.Length; i++)
+for (int i = 0; i < byuser.Length; i++)
 {
-    if (arr[i].Length <= 3)
+    if (byuser[i].Length <= 3)
     {
-        list.Add(arr[i]);
+        list.Add(byuser[i]);
     }
 }
 
